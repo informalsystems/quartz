8603c9753471cc2d46e302ca8cbcae2f04b9563d
hu55a1n1 <sufialhussaini@gmail.com>
2023-11-29 13:37:02 -0800

    Store requests in a map

--- bisenzone-cw-mvp/contracts/cw-tee-mtcs/src/msg.rs
+++ bisenzone-cw-mvp/contracts/cw-tee-mtcs/src/msg.rs
@@ -6,6 +6,25 @@ pub struct InstantiateMsg;
 #[cw_serde]
 pub enum ExecuteMsg {}
 
+pub mod execute {
+    use super::*;
+
+    #[cw_serde]
+    pub struct Nonce([u8; 32]);
+
+    #[cw_serde]
+    pub struct JoinComputeNodeMsg {
+        compute_node_pub_key: String,
+        nonce: Nonce,
+    }
+
+    #[cw_serde]
+    pub struct ShareEpochKeyMsg {
+        compute_node_pub_key: String,
+        nonce: Nonce,
+    }
+}
+
 #[cw_serde]
 #[derive(QueryResponses)]
 pub enum QueryMsg {}
--- bisenzone-cw-mvp/contracts/cw-tee-mtcs/src/state.rs
+++ bisenzone-cw-mvp/contracts/cw-tee-mtcs/src/state.rs
@@ -1,9 +1,17 @@
+use crate::msg::execute::{JoinComputeNodeMsg, Nonce, ShareEpochKeyMsg};
 use cosmwasm_schema::cw_serde;
-use cw_storage_plus::Item;
+use cw_storage_plus::{Item, Map};
 
 #[cw_serde]
 pub struct State {
     pub owner: String,
 }
 
+#[cw_serde]
+pub enum Request {
+    JoinComputeNode(JoinComputeNodeMsg),
+    ShareEpochKey(ShareEpochKeyMsg),
+}
+
 pub const STATE: Item<State> = Item::new("state");
+pub const REQUESTS: Map<&Nonce, &Request> = Map::new("requests");
