8c0c8716661d19c36af2d42e2f06d286c0a7dc86
hu55a1n1 <sufialhussaini@gmail.com>
2024-01-04 03:26:51 -0800

    Fix proof gen and verify

--- utils/tm-prover/src/main.rs
+++ utils/tm-prover/src/main.rs
@@ -180,19 +180,20 @@ async fn main() -> Result<()> {
         .latest_trusted()
         .ok_or_else(|| eyre!("No trusted state found for primary"))?;
 
-    let primary_block = {
-        info!("Verifying to latest height on primary...");
+    let status = client.status().await?;
+    let latest_height = status.sync_info.latest_block_height;
 
-        let status = client.status().await?;
-        let proof_height = {
-            let latest_height = status.sync_info.latest_block_height;
-            (latest_height.value() - 1)
-                .try_into()
-                .expect("infallible conversion")
-        };
+    // `proof_height` is the height at which we want to query the blockchain's state
+    // This is one less than than the `latest_height` because we want to verify the merkle-proof for
+    // the state against the `app_hash` at `latest_height`.
+    // (because Tendermint commits to the latest `app_hash` in the subsequent block)
+    let proof_height = (latest_height.value() - 1)
+        .try_into()
+        .expect("infallible conversion");
 
-        primary.verify_to_height(proof_height)
-    }?;
+    info!("Verifying to latest height on primary...");
+
+    let primary_block = primary.verify_to_height(latest_height)?;
 
     info!("Verified to height {} on primary", primary_block.height());
     let mut primary_trace = primary.get_trace(primary_block.height());
@@ -223,8 +224,7 @@ async fn main() -> Result<()> {
     .await?;
 
     let status = client.status().await?;
-    let (proof_height, latest_app_hash) =
-        (primary_block.height(), status.sync_info.latest_app_hash);
+    let latest_app_hash = status.sync_info.latest_app_hash;
 
     let path = WASM_STORE_KEY.to_owned();
     let data = CwAbciKey::new(args.contract_address, args.storage_key, None);
