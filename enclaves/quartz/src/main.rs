3a201cca82afb9a5dbbb53d7936e4d7f2c00c219
hu55a1n1 <sufialhussaini@gmail.com>
2024-02-22 12:48:13 -0800

    Impl config + CLI

--- enclaves/quartz/src/main.rs
+++ enclaves/quartz/src/main.rs
@@ -14,21 +14,39 @@
     unused_qualifications
 )]
 
+mod cli;
 mod server;
 
+use std::time::Duration;
+
+use clap::Parser;
 use quartz_proto::quartz::core_server::CoreServer;
 use tonic::transport::Server;
 
-use crate::server::CoreService;
+use crate::{
+    cli::Cli,
+    server::{Config, CoreService, LightClientOpts},
+};
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "127.0.0.1:11090".parse()?;
-    let core_service = CoreService::default();
+    let args = Cli::parse();
+
+    let light_client_opts = LightClientOpts::new(
+        args.chain_id,
+        args.target_height,
+        args.trusted_height,
+        args.trusted_hash,
+        args.trust_threshold,
+        args.trusting_period,
+        args.max_clock_drift,
+        args.max_block_lag,
+    );
+    let config = Config::new(Duration::from_secs(30 * 24 * 60), light_client_opts);
 
     Server::builder()
-        .add_service(CoreServer::new(core_service))
-        .serve(addr)
+        .add_service(CoreServer::new(CoreService(config)))
+        .serve(args.rpc_addr)
         .await?;
 
     Ok(())
--- enclaves/quartz/src/server.rs
+++ enclaves/quartz/src/server.rs
@@ -1,11 +1,16 @@
+use std::time::Duration;
+
 use quartz_proto::quartz::{
     core_server::Core, InstantiateRequest, InstantiateResponse, SessionCreateRequest,
     SessionCreateResponse,
 };
+use serde::{Deserialize, Serialize};
+use tendermint::Hash;
+use tendermint_light_client::types::{Height, TrustThreshold};
 use tonic::{Request, Response, Status};
 
-#[derive(Debug, Default)]
-pub struct CoreService;
+#[derive(Clone, Debug)]
+pub struct CoreService(pub Config);
 
 #[tonic::async_trait]
 impl Core for CoreService {
@@ -32,5 +37,57 @@ impl Core for CoreService {
         };
 
         Ok(Response::new(reply))
+    }
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Config {
+    epoch_duration: Duration,
+    light_client_opts: LightClientOpts,
+}
+
+impl Config {
+    pub fn new(epoch_duration: Duration, light_client_opts: LightClientOpts) -> Self {
+        Self {
+            epoch_duration,
+            light_client_opts,
+        }
+    }
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct LightClientOpts {
+    chain_id: String,
+    target_height: Height,
+    trusted_height: Height,
+    trusted_hash: Hash,
+    trust_threshold: TrustThreshold,
+    trusting_period: u64,
+    max_clock_drift: u64,
+    max_block_lag: u64,
+}
+
+impl LightClientOpts {
+    #[allow(clippy::too_many_arguments)]
+    pub fn new(
+        chain_id: String,
+        target_height: Height,
+        trusted_height: Height,
+        trusted_hash: Hash,
+        trust_threshold: TrustThreshold,
+        trusting_period: u64,
+        max_clock_drift: u64,
+        max_block_lag: u64,
+    ) -> Self {
+        Self {
+            chain_id,
+            target_height,
+            trusted_height,
+            trusted_hash,
+            trust_threshold,
+            trusting_period,
+            max_clock_drift,
+            max_block_lag,
+        }
     }
 }
