054e77d6937d024706fc9b69cb67adb4b013199a
hu55a1n1 <sufialhussaini@gmail.com>
2024-02-22 12:09:44 -0800

    Impl instantiate in server.rs

--- enclaves/quartz/src/server.rs
+++ enclaves/quartz/src/server.rs
@@ -1,11 +1,26 @@
-use quartz_proto::quartz::{core_server::Core, SessionCreateRequest, SessionCreateResponse};
+use quartz_proto::quartz::{
+    core_server::Core, InstantiateRequest, InstantiateResponse, SessionCreateRequest,
+    SessionCreateResponse,
+};
 use tonic::{Request, Response, Status};
 
 #[derive(Debug, Default)]
-pub struct CoreService {}
+pub struct CoreService;
 
 #[tonic::async_trait]
 impl Core for CoreService {
+    async fn instantiate(
+        &self,
+        request: Request<InstantiateRequest>,
+    ) -> Result<Response<InstantiateResponse>, Status> {
+        println!("Got a request: {:?}", request);
+
+        let reply = InstantiateResponse {
+            message: "Hello!".to_string(),
+        };
+
+        Ok(Response::new(reply))
+    }
     async fn session_create(
         &self,
         request: Request<SessionCreateRequest>,
