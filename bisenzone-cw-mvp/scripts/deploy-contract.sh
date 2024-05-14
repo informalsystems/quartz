103a4631661395697040175b6ced532b9f1524d6
Ethan Buchman <ethan@coinculture.info>
2024-05-14 12:10:43 -0400

    deploy-contract.sh uses $NODE for address (#31)
    Co-authored-by: hu55a1n1 <sufialhussaini@gmail.com>

--- bisenzone-cw-mvp/scripts/deploy-contract.sh
+++ bisenzone-cw-mvp/scripts/deploy-contract.sh
@@ -23,24 +23,27 @@ fi
 USER_ADDR=${USER_ADDR:-$(wasmd keys show -a admin)}
 WASM_BIN="$1"
 CHAIN_ID=${CHAIN_ID:-testing}
+NODE_URL=${NODE_URL:-127.0.0.1:26657}
 LABEL=${LABEL:-bisenzone-mvp}
 COUNT=${COUNT:-0}
 INSTANTIATE_MSG=${INSTANTIATE_MSG:-"{}"}
 
 TXFLAG="--chain-id ${CHAIN_ID} --gas-prices 0.0025ucosm --gas auto --gas-adjustment 1.3"
 
+CMD="wasmd --node http://$NODE_URL"
+
 echo "🚀 Deploying WASM contract '${WASM_BIN}' on chain '${CHAIN_ID}' using account '${USER_ADDR}'..."
 echo "===================================================================="
 
-RES=$(wasmd tx wasm store "$WASM_BIN" --from "$USER_ADDR" $TXFLAG -y --output json)
+RES=$($CMD tx wasm store "$WASM_BIN" --from "$USER_ADDR" $TXFLAG -y --output json)
 TX_HASH=$(echo $RES | jq -r '.["txhash"]')
 
-while ! wasmd query tx $TX_HASH &> /dev/null; do
+while ! $CMD query tx $TX_HASH &> /dev/null; do
     echo "... 🕐 waiting for contract to deploy"
     sleep 1
 done
 
-RES=$(wasmd query tx "$TX_HASH" --output json)
+RES=$($CMD query tx "$TX_HASH" --output json)
 CODE_ID=$(echo $RES | jq -r '.logs[0].events[1].attributes[1].value')
 
 echo ""
@@ -49,17 +52,17 @@ echo "--------------------------------------------------------"
 echo "Label: ${LABEL}"
 echo "--------------------------------------------------------"
 
-RES=$(wasmd tx wasm instantiate "$CODE_ID" "$INSTANTIATE_MSG" --from "$USER_ADDR" --label $LABEL $TXFLAG -y --no-admin --output json)
+RES=$($CMD tx wasm instantiate "$CODE_ID" "$INSTANTIATE_MSG" --from "$USER_ADDR" --label $LABEL $TXFLAG -y --no-admin --output json)
 TX_HASH=$(echo $RES | jq -r '.["txhash"]')
 
 
 echo ""
-while ! wasmd query tx $TX_HASH &> /dev/null; do
+while ! $CMD query tx $TX_HASH &> /dev/null; do
     echo "... 🕐 waiting for contract to be queryable"
     sleep 1
 done
 
-RES=$(wasmd query wasm list-contract-by-code "$CODE_ID" --output json)
+RES=$($CMD query wasm list-contract-by-code "$CODE_ID" --output json)
 CONTRACT=$(echo $RES | jq -r '.contracts[0]')
 
 echo "🚀 Successfully deployed and instantiated contract!"
