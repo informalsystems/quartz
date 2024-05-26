52d34f0aba3cc2737681698d1e11cc93bed45f34
hu55a1n1 <sufialhussaini@gmail.com>
2024-05-27 01:34:15 +0200

    Ctor for TlsCertificateChainVerifier

--- cosmwasm/packages/quartz-tee-ra/src/intel_sgx/dcap/certificate_chain.rs
+++ cosmwasm/packages/quartz-tee-ra/src/intel_sgx/dcap/certificate_chain.rs
@@ -5,6 +5,13 @@ use x509_cert::{crl::CertificateList, Certificate};
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize, Eq, PartialEq)]
 pub struct TlsCertificateChainVerifier;
 
+impl TlsCertificateChainVerifier {
+    pub fn new(_root_ca: &str) -> Self {
+        // FIXME(hu55a1n1)
+        Self
+    }
+}
+
 impl CertificateChainVerifier for TlsCertificateChainVerifier {
     fn verify_certificate_chain<'a, 'b>(
         &self,
