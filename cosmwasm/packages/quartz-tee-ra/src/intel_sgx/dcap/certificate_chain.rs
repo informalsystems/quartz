e880c87a7544c9639641100346e7c7ae7dedbe5a
hu55a1n1 <sufialhussaini@gmail.com>
2024-05-27 12:34:04 +0200

    Add tests for cert chain validation

--- cosmwasm/packages/quartz-tee-ra/src/intel_sgx/dcap/certificate_chain.rs
+++ cosmwasm/packages/quartz-tee-ra/src/intel_sgx/dcap/certificate_chain.rs
@@ -22,3 +22,67 @@ impl CertificateChainVerifier for TlsCertificateChainVerifier {
         todo!()
     }
 }
+
+#[cfg(test)]
+mod test {
+    use der::{Decode, DecodePem};
+
+    use super::*;
+
+    const LEAF_CERT: &str = include_str!("../../../data/leaf_cert.pem");
+    const PROCESSOR_CA: &str = include_str!("../../../data/processor_ca.pem");
+    const ROOT_CA: &str = include_str!("../../../data/root_ca.pem");
+    const PROCESSOR_CRL: &[u8] = include_bytes!("../../../data/processor_crl.der");
+    const ROOT_CRL: &[u8] = include_bytes!("../../../data/root_crl.der");
+
+    #[test]
+    fn verify_valid_cert_chain() {
+        let chain = [LEAF_CERT, PROCESSOR_CA, ROOT_CA]
+            .iter()
+            .map(|cert| Certificate::from_pem(cert).expect("failed to parse cert"))
+            .collect::<Vec<_>>();
+        let crls = [ROOT_CRL, PROCESSOR_CRL]
+            .iter()
+            .map(|crl| CertificateList::from_der(crl).expect("failed to parse CRL"))
+            .collect::<Vec<_>>();
+        let verifier = TlsCertificateChainVerifier::new(ROOT_CA);
+        assert!(verifier
+            .verify_certificate_chain(chain.iter(), crls.iter(), None)
+            .is_ok());
+    }
+
+    #[test]
+    fn invalid_cert_chain() {
+        let chain = [LEAF_CERT, ROOT_CA]
+            .iter()
+            .map(|cert| Certificate::from_pem(cert).expect("failed to parse cert"))
+            .collect::<Vec<_>>();
+        let crls = [ROOT_CRL, PROCESSOR_CRL]
+            .iter()
+            .map(|crl| CertificateList::from_der(crl).expect("failed to parse CRL"))
+            .collect::<Vec<_>>();
+        let verifier = TlsCertificateChainVerifier::new(ROOT_CA);
+        assert_eq!(
+            verifier.verify_certificate_chain(chain.iter(), crls.iter(), None),
+            Err(CertificateChainVerifierError::SignatureVerification)
+        );
+    }
+
+    #[test]
+    fn unordered_cert_chain_succeeds() {
+        let chain = [PROCESSOR_CA, ROOT_CA, LEAF_CERT]
+            .iter()
+            .map(|cert| Certificate::from_pem(cert).expect("failed to parse cert"))
+            .collect::<Vec<_>>();
+        let crls = [ROOT_CRL, PROCESSOR_CRL]
+            .iter()
+            .map(|crl| CertificateList::from_der(crl).expect("failed to parse CRL"))
+            .collect::<Vec<_>>();
+        let verifier = TlsCertificateChainVerifier::new(ROOT_CA);
+        assert!(verifier
+            .verify_certificate_chain(chain.iter(), crls.iter(), None)
+            .is_ok());
+    }
+
+    // TODO(hu55a1n1) - add [PKITS tests](https://csrc.nist.gov/projects/pki-testing)
+}
