f1c5495554ec72f1dbae3cc4a574a278622f1f79
hu55a1n1 <sufialhussaini@gmail.com>
2024-07-02 21:55:29 +0200

    Clippy fix

--- apps/transfers/enclave/bin/encrypt.rs
+++ apps/transfers/enclave/bin/encrypt.rs
@@ -1,12 +1,8 @@
-use std::collections::{BTreeMap, HashMap};
+use std::collections::BTreeMap;
 
-use anyhow;
 use cosmwasm_std::{Addr, HexBinary, Uint128};
-use ecies::{decrypt, encrypt};
-use k256::{
-    ecdsa::{SigningKey, VerifyingKey},
-    pkcs8::DecodePublicKey,
-};
+use ecies::encrypt;
+use k256::ecdsa::VerifyingKey;
 use serde::{Deserialize, Serialize};
 use transfers_contracts::msg::execute::ClearTextTransferRequestMsg;
 
@@ -45,7 +41,7 @@ fn main() {
     let msg = ClearTextTransferRequestMsg {
         sender: Addr::unchecked("alice"),
         receiver: Addr::unchecked("bob"),
-        amount: Uint128::from(100 as u32),
+        amount: Uint128::from(100_u32),
     };
 
     let decoded: Vec<u8> =
