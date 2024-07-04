c4c44011ec7b068f5f38b0dcad54ebf5e31d87d1
Peppi Littera <giuseppe@informal.systems>
2024-07-04 12:02:53 +0200

    Update to `cw-tee-mtcs`

--- apps/mtcs/contracts/cw-tee-mtcs/src/contract.rs
+++ apps/mtcs/contracts/cw-tee-mtcs/src/contract.rs
@@ -1,6 +1,6 @@
 use cosmwasm_std::{
     entry_point, to_json_binary, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult,
-    Uint128,
+    Uint128, Uint64,
 };
 use cw2::set_contract_version;
 use cw20_base::{
@@ -19,8 +19,7 @@ use crate::{
         ExecuteMsg, InstantiateMsg, QueryMsg,
     },
     state::{
-        current_epoch_key, LiquiditySourcesItem, ObligationsItem, State, LIQUIDITY_SOURCES_KEY,
-        OBLIGATIONS_KEY, STATE,
+        current_epoch_key, State, LIQUIDITY_SOURCES, LIQUIDITY_SOURCES_KEY, OBLIGATIONS, OBLIGATIONS_KEY, STATE
     },
 };
 
@@ -42,15 +41,21 @@ pub fn instantiate(
         owner: info.sender.to_string(),
     };
     set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
-    STATE.save(deps.storage, &state)?;
+    STATE.save(deps.storage, &state)?;       
+    
+    
+    let epoch_counter = Uint64::new(1);
+    EPOCH_COUNTER.save(deps.storage, &epoch_counter)?;
 
-    EPOCH_COUNTER.save(deps.storage, &1)?;
+    // Pre-compute the keys
+    let obligations_key = current_epoch_key(OBLIGATIONS_KEY, deps.storage)?;
+    let liquidity_sources_key = current_epoch_key(LIQUIDITY_SOURCES_KEY, deps.storage)?;
 
-    ObligationsItem::new(&current_epoch_key(OBLIGATIONS_KEY, deps.storage)?)
-        .save(deps.storage, &Default::default())?;
+    // Now use the pre-computed keys
+    OBLIGATIONS.save(deps.storage, &obligations_key, &Default::default())?;
+    LIQUIDITY_SOURCES.save(deps.storage, &liquidity_sources_key, &Default::default())?;
 
-    LiquiditySourcesItem::new(&current_epoch_key(LIQUIDITY_SOURCES_KEY, deps.storage)?)
-        .save(deps.storage, &Default::default())?;
+
 
     // store token info using cw20-base format
     let data = TokenInfo {
@@ -116,15 +121,14 @@ pub fn execute(
 pub mod execute {
     use std::collections::BTreeMap;
 
-    use cosmwasm_std::{DepsMut, Env, HexBinary, MessageInfo, Response, StdResult};
+    use cosmwasm_std::{DepsMut, Env, HexBinary, MessageInfo, Response, StdResult, Uint64};
     use cw20_base::contract::{execute_burn, execute_mint};
     use k256::ecdsa::VerifyingKey;
     use quartz_cw::state::{Hash, EPOCH_COUNTER};
 
     use crate::{
         state::{
-            current_epoch_key, previous_epoch_key, LiquiditySourcesItem, ObligationsItem, RawHash,
-            SetoffsItem, SettleOff, LIQUIDITY_SOURCES_KEY, OBLIGATIONS_KEY, SETOFFS_KEY,
+            current_epoch_key, previous_epoch_key, RawHash,  SettleOff, LIQUIDITY_SOURCES, LIQUIDITY_SOURCES_KEY, OBLIGATIONS_KEY, SETOFFS, SETOFFS_KEY
         },
         ContractError,
     };
@@ -150,6 +154,7 @@ pub mod execute {
 
         Ok(Response::new().add_attribute("action", "faucet_mint"))
     }
+    use crate::state::OBLIGATIONS;
 
     pub fn submit_obligation(
         deps: DepsMut,
@@ -157,18 +162,22 @@ pub mod execute {
         digest: HexBinary,
     ) -> Result<Response, ContractError> {
         let _: Hash = digest.to_array()?;
-
+    
+        let current_obligation_key = current_epoch_key(OBLIGATIONS_KEY, deps.storage)?;
+    
         // store the `(digest, ciphertext)` tuple
-        ObligationsItem::new(&current_epoch_key(OBLIGATIONS_KEY, deps.storage)?).update(
+        OBLIGATIONS.update(
             deps.storage,
-            |mut obligations| {
+            &current_obligation_key,
+            |obligations| -> Result<_, ContractError> {
+                let mut obligations = obligations.unwrap_or_default();
                 if let Some(_duplicate) = obligations.insert(digest.clone(), ciphertext.clone()) {
                     return Err(ContractError::DuplicateEntry);
                 }
                 Ok(obligations)
             },
         )?;
-
+    
         Ok(Response::new()
             .add_attribute("action", "submit_obligation")
             .add_attribute("digest", digest.to_string())
@@ -183,34 +192,32 @@ pub mod execute {
         liquidity_sources
             .iter()
             .try_for_each(|ls| VerifyingKey::from_sec1_bytes(ls).map(|_| ()))?;
-
+    
+        let current_liquidity_key = current_epoch_key(LIQUIDITY_SOURCES_KEY, deps.storage)?;
+    
         // store the liquidity sources
-        LiquiditySourcesItem::new(&current_epoch_key(LIQUIDITY_SOURCES_KEY, deps.storage)?)
-            .update(deps.storage, |mut ls| {
-                ls.clear();
-                ls.extend(liquidity_sources);
-                Ok::<_, ContractError>(ls)
-            })?;
-
+        let liquidity_sources_set: std::collections::BTreeSet<_> = liquidity_sources.into_iter().collect();
+        LIQUIDITY_SOURCES.save(deps.storage, &current_liquidity_key, &liquidity_sources_set)?;
+    
         Ok(Response::default())
     }
-
+   
     pub fn submit_setoffs(
         mut deps: DepsMut,
         env: Env,
         setoffs_enc: BTreeMap<RawHash, SettleOff>,
     ) -> Result<Response, ContractError> {
         // store the `BTreeMap<RawHash, RawCipherText>`
-        SetoffsItem::new(&previous_epoch_key(SETOFFS_KEY, deps.storage)?)
-            .save(deps.storage, &setoffs_enc)?;
-
+        let previous_epoch_key = previous_epoch_key(SETOFFS_KEY, deps.storage)?;
+        SETOFFS.save(deps.storage, &previous_epoch_key, &setoffs_enc)?;
+    
         for (_, so) in setoffs_enc {
             if let SettleOff::Transfer(t) = so {
                 let info = MessageInfo {
                     sender: env.contract.address.clone(),
                     funds: vec![],
                 };
-
+    
                 execute_mint(
                     deps.branch(),
                     env.clone(),
@@ -218,23 +225,23 @@ pub mod execute {
                     t.payee.to_string(),
                     t.amount.into(),
                 )?;
-
+    
                 let payer = deps.api.addr_validate(&t.payer.to_string())?;
                 let info = MessageInfo {
                     sender: payer,
                     funds: vec![],
                 };
-
+    
                 execute_burn(deps.branch(), env.clone(), info, t.amount.into())?;
             }
         }
-
+    
         Ok(Response::new().add_attribute("action", "submit_setoffs"))
     }
 
     pub fn init_clearing(deps: DepsMut) -> Result<Response, ContractError> {
         EPOCH_COUNTER.update(deps.storage, |mut counter| -> StdResult<_> {
-            counter += 1;
+            counter = counter.saturating_add(Uint64::from(1u64));
             Ok(counter)
         })?;
         Ok(Response::new().add_attribute("action", "init_clearing"))
@@ -253,37 +260,34 @@ pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
 }
 
 pub mod query {
-    use cosmwasm_std::{Deps, StdResult};
+    use cosmwasm_std::{Deps, StdResult, Uint64};
 
     use crate::{
         msg::{GetAllSetoffsResponse, GetLiquiditySourcesResponse},
         state::{
-            current_epoch_key, epoch_key, previous_epoch_key, LiquiditySourcesItem, SetoffsItem,
+            current_epoch_key, epoch_key, previous_epoch_key, LIQUIDITY_SOURCES, SETOFFS,
             LIQUIDITY_SOURCES_KEY, SETOFFS_KEY,
         },
     };
 
     pub fn get_all_setoffs(deps: Deps) -> StdResult<GetAllSetoffsResponse> {
-        let setoffs = SetoffsItem::new(&previous_epoch_key(SETOFFS_KEY, deps.storage)?)
-            .load(deps.storage)?
-            .into_iter()
-            .collect();
+        let previous_epoch_key = previous_epoch_key(SETOFFS_KEY, deps.storage)?;
+        let setoffs_map = SETOFFS.load(deps.storage, &previous_epoch_key)?;
+        let setoffs = setoffs_map.into_iter().collect();
         Ok(GetAllSetoffsResponse { setoffs })
     }
 
     pub fn get_liquidity_sources(
         deps: Deps,
-        epoch: Option<usize>,
+        epoch: Option<Uint64>,
     ) -> StdResult<GetLiquiditySourcesResponse> {
         let epoch_key = match epoch {
             None => current_epoch_key(LIQUIDITY_SOURCES_KEY, deps.storage)?,
             Some(e) => epoch_key(LIQUIDITY_SOURCES_KEY, e)?,
         };
-
-        let liquidity_sources = LiquiditySourcesItem::new(&epoch_key)
-            .load(deps.storage)?
-            .into_iter()
-            .collect();
+    
+        let liquidity_sources_set = LIQUIDITY_SOURCES.load(deps.storage, &epoch_key)?;
+        let liquidity_sources = liquidity_sources_set.into_iter().collect();
         Ok(GetLiquiditySourcesResponse { liquidity_sources })
     }
 }
--- apps/mtcs/contracts/cw-tee-mtcs/src/state.rs
+++ apps/mtcs/contracts/cw-tee-mtcs/src/state.rs
@@ -1,16 +1,21 @@
 use std::collections::{BTreeMap, BTreeSet};
 
 use cosmwasm_schema::cw_serde;
-use cosmwasm_std::{HexBinary, StdError, Storage};
-use cw_storage_plus::Item;
+use cosmwasm_std::{HexBinary, StdError, Storage, Uint64};
+use cw_storage_plus::{Item, Map};
 use quartz_cw::state::EPOCH_COUNTER;
 
 pub type RawHash = HexBinary;
 pub type RawCipherText = HexBinary;
 
-pub type ObligationsItem<'a> = Item<'a, BTreeMap<RawHash, RawCipherText>>;
-pub type SetoffsItem<'a> = Item<'a, BTreeMap<RawHash, SettleOff>>;
-pub type LiquiditySourcesItem<'a> = Item<'a, BTreeSet<HexBinary>>;
+pub type ObligationsItem = Item<BTreeMap<RawHash, RawCipherText>>;
+pub type SetoffsItem = Item<BTreeMap<RawHash, SettleOff>>;
+pub type LiquiditySourcesItem = Item<BTreeSet<HexBinary>>;
+
+
+pub const OBLIGATIONS: Map<&str, BTreeMap<RawHash, RawCipherText>> = Map::new("obligations");
+pub const SETOFFS: Map<&str, BTreeMap<RawHash, SettleOff>> = Map::new("setoffs");
+pub const LIQUIDITY_SOURCES: Map<&str, BTreeSet<HexBinary>> = Map::new("liquidity_sources");
 
 #[cw_serde]
 pub struct State {
@@ -36,14 +41,33 @@ pub const OBLIGATIONS_KEY: &str = "obligations";
 pub const SETOFFS_KEY: &str = "setoffs";
 pub const LIQUIDITY_SOURCES_KEY: &str = "liquidity_sources";
 
+// pub fn current_epoch_key(key: &str, storage: &dyn Storage) -> Result<String, StdError> {
+//     epoch_key(key, EPOCH_COUNTER.load(storage)?)
+// }
+
+// pub fn previous_epoch_key(key: &str, storage: &dyn Storage) -> Result<String, StdError> {
+//     epoch_key(key, EPOCH_COUNTER.load(storage)? - 1)
+// }
+
+// pub fn epoch_key(key: &str, epoch: usize) -> Result<String, StdError> {
+//     Ok(format!("{}/{key}", epoch))
+// }
+
+
 pub fn current_epoch_key(key: &str, storage: &dyn Storage) -> Result<String, StdError> {
-    epoch_key(key, EPOCH_COUNTER.load(storage)?)
+    let epoch = EPOCH_COUNTER.load(storage)?;
+    epoch_key(key, epoch.into())
 }
 
 pub fn previous_epoch_key(key: &str, storage: &dyn Storage) -> Result<String, StdError> {
-    epoch_key(key, EPOCH_COUNTER.load(storage)? - 1)
+    let epoch = EPOCH_COUNTER.load(storage)?;
+    if epoch == Uint64::zero() {
+        return Err(StdError::generic_err("Cannot get previous epoch for epoch 0"));
+    }
+    epoch_key(key, epoch - Uint64::new(1))
 }
 
-pub fn epoch_key(key: &str, epoch: usize) -> Result<String, StdError> {
-    Ok(format!("{}/{key}", epoch))
+pub fn epoch_key(key: &str, epoch: Uint64) -> Result<String, StdError> {
+    Ok(format!("{}/{}", epoch, key))
 }
+
