9816052b4f5316ab06d8119afa477871d2fd7a5f
David Kajpust <davidkajpust@davids-mbp.home>
2024-07-11 21:23:06 -0400

    Fix

--- apps/transfers/contracts/src/contract.rs
+++ apps/transfers/contracts/src/contract.rs
@@ -60,11 +60,13 @@ pub fn execute(
 
         // Enclave msgs
         ExecuteMsg::Update(attested_msg) => {
-            let _ = attested_msg
+            // let _ = attested_msg
+            attested_msg
                 .clone()
-                .handle_raw(deps.branch(), &env, &info)?;
-            let UpdateMsg { ciphertext, quantity, withdrawals } = attested_msg.msg.0;
-            update(deps, env, info, UpdateMsg { ciphertext, quantity, withdrawals })
+                .handle_raw(deps.branch(), &env, &info).map_err(Into::into)
+                // .handle_raw(deps.branch(), &env, &info)?;
+            // let UpdateMsg { ciphertext, quantity, withdrawals } = attested_msg.msg.0;
+            // update(deps, env, info, UpdateMsg { ciphertext, quantity, withdrawals })
         }
         ExecuteMsg::QueryResponse(attested_msg) => {
             let _ = attested_msg
