f57cea3b99f1b10dd54617c4726c67da677dca1e
Ethan Buchman <ethan@coinculture.info>
2024-07-16 18:49:54 -0400

    fixes for mac and debugging

--- apps/transfers/scripts/deploy.sh
+++ apps/transfers/scripts/deploy.sh
@@ -9,12 +9,13 @@ echo "--------------------------------------------------------"
 echo "instantiate"
 cd  $ROOT/cycles-quartz/relayer/
 export INSTANTIATE_MSG=$(./scripts/relay.sh Instantiate | jq '{quartz: .} + {denom: "ucosm"}' )
+echo $INSTANTIATE_MSG
 echo "--------------------------------------------------------"
 
 echo "deploy contract"
 cd $ROOT/cycles-quartz/apps/transfers/contracts/
 
-bash deploy-contract.sh target/wasm32-unknown-unknown/release/transfers_contract.wasm  |& tee output
+bash deploy-contract.sh target/wasm32-unknown-unknown/release/transfers_contract.wasm  2>&1 | tee output
 export CONTRACT=$(cat output | grep Address | awk '{print $NF}' | sed 's/\x1b\[[0-9;]*m//g')
 echo $CONTRACT 
 
