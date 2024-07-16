cd065c92cbfad85241315e0c933d079ad2ba28a1
hu55a1n1 <sufialhussaini@gmail.com>
2024-07-16 23:25:23 +0200

    Replace transfers_contracts -> transfers_contract

--- apps/transfers/scripts/deploy.sh
+++ apps/transfers/scripts/deploy.sh
@@ -14,7 +14,7 @@ echo "--------------------------------------------------------"
 echo "deploy contract"
 cd $ROOT/cycles-quartz/apps/transfers/contracts/
 
-bash deploy-contract.sh target/wasm32-unknown-unknown/release/transfers_contracts.wasm  |& tee output
+bash deploy-contract.sh target/wasm32-unknown-unknown/release/transfers_contract.wasm  |& tee output
 export CONTRACT=$(cat output | grep Address | awk '{print $NF}' | sed 's/\x1b\[[0-9;]*m//g')
 echo $CONTRACT 
 
