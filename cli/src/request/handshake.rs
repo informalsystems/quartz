8b92ae3075aa5b9ed29fbcfc34d1c0e3ebfe12cd
Daniel Gushchyan <d.gushchyan@gmail.com>
2024-07-31 15:03:19 -0700

    remove comment

--- cli/src/request/handshake.rs
+++ cli/src/request/handshake.rs
@@ -15,21 +15,6 @@ pub struct HandshakeRequest {
     pub path: PathBuf,
 }
 
-// TODO: Would I ever need this?
-// impl TryFrom<HandshakeRequest> for HandshakeRequest {
-//     type Error = Error;
-
-//     fn try_from(path: Option<PathBuf>) -> Result<Self, Self::Error> {
-//         if let Some(path) = path {
-//             if !path.is_dir() {
-//                 return Err(Error::PathNotDir(format!("{}", path.display())));
-//             }
-//         }
-
-//         todo!()
-//     }
-// }
-
 impl From<HandshakeRequest> for Request {
     fn from(request: HandshakeRequest) -> Self {
         Self::Handshake(request)
