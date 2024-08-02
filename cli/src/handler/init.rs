3f1cd0b4634eff673e88885212056e8cff52176f
Daniel Gushchyan <39884512+dangush@users.noreply.github.com>
2024-08-02 12:19:07 -0700

    feat: cli enclave build (#137)

--- cli/src/handler/init.rs
+++ cli/src/handler/init.rs
@@ -1,15 +1,14 @@
 use tracing::trace;
 
 use crate::{
-    cli::Verbosity, error::Error, handler::Handler, request::init::InitRequest,
-    response::init::InitResponse,
+    error::Error, handler::Handler, request::init::InitRequest, response::Response, Config,
 };
 
 impl Handler for InitRequest {
     type Error = Error;
-    type Response = InitResponse;
+    type Response = Response;
 
-    fn handle(self, _verbosity: Verbosity) -> Result<Self::Response, Self::Error> {
+    fn handle(self, _config: Config) -> Result<Self::Response, Self::Error> {
         trace!("initializing directory structure...");
         todo!()
     }
--- cli/src/request.rs
+++ cli/src/request.rs
@@ -1,10 +1,16 @@
-use crate::{cli::Command, error::Error, request::init::InitRequest};
+use crate::{
+    cli::{Command, EnclaveCommand},
+    error::Error,
+    request::{enclave_build::EnclaveBuildRequest, init::InitRequest},
+};
 
+pub mod enclave_build;
 pub mod init;
 
 #[derive(Clone, Debug)]
 pub enum Request {
     Init(InitRequest),
+    EnclaveBuild(EnclaveBuildRequest),
 }
 
 impl TryFrom<Command> for Request {
@@ -12,8 +18,13 @@ impl TryFrom<Command> for Request {
 
     fn try_from(cmd: Command) -> Result<Self, Self::Error> {
         match cmd {
-            Command::Init { path } => InitRequest::try_from(path),
+            Command::Init { path } => InitRequest::try_from(path).map(Into::into),
+            Command::Enclave { enclave_command } => match enclave_command {
+                EnclaveCommand::Build { manifest_path } => {
+                    Ok(EnclaveBuildRequest { manifest_path }.into())
+                }
+                _ => todo!(),
+            },
         }
-        .map(Into::into)
     }
 }
