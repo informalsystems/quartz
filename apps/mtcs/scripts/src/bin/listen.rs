fdb8ccf69552dfd15911a2b5bdfdecaec9383156
hu55a1n1 <sufialhussaini@gmail.com>
2024-08-07 23:32:48 +0200

    Undo redundant changes

--- apps/mtcs/scripts/src/bin/listen.rs
+++ apps/mtcs/scripts/src/bin/listen.rs
@@ -1,33 +1,26 @@
-use core::fmt;
-use std::{
-    collections::{BTreeMap, BTreeSet},
-    env,
-    process::Command,
-    str::FromStr,
-};
+use std::{collections::BTreeMap, env, process::Command, str::FromStr};
 
 use anyhow::anyhow;
 use base64::prelude::*;
 use clap::Parser;
 use cosmrs::{tendermint::chain::Id as ChainId, AccountId};
-use cosmwasm_std::{HexBinary, Uint64};
-use cw_tee_mtcs::{
-    msg::{
-        execute::SubmitSetoffsMsg, AttestedMsg, ExecuteMsg, GetLiquiditySourcesResponse,
-        QueryMsg::GetLiquiditySources,
-    },
-    state::LiquiditySource,
+use cosmwasm_std::{Binary, HexBinary, Uint64};
+use cw_tee_mtcs::msg::{
+    execute::SubmitSetoffsMsg, AttestedMsg, ExecuteMsg, GetLiquiditySourcesResponse,
+    QueryMsg::GetLiquiditySources,
 };
 use cycles_sync::wasmd_client::{CliWasmdClient, QueryResult, WasmdClient};
 use futures_util::stream::StreamExt;
-use mtcs_enclave::proto::{clearing_client::ClearingClient, RunClearingRequest};
-use quartz_common::contract::msg::execute::attested::{
-    DcapAttestation, Quote, RawAttested, RawAttestedMsgSansHandler,
+use mtcs_enclave::{
+    proto::{clearing_client::ClearingClient, RunClearingRequest},
+    RunClearingMessage,
 };
-use quartz_tee_ra::intel_sgx::dcap::{sgx_ql_qve_collateral_t, Collateral, Quote3, Quote3Error};
+use quartz_common::contract::msg::execute::attested::{
+    EpidAttestation, RawAttested, RawAttestedMsgSansHandler, RawEpidAttestation,
+};
+use quartz_tee_ra::{intel_sgx::epid::types::ReportBody, IASReport};
 use reqwest::Url;
 use scripts::utils::wasmaddr_to_id;
-use serde::{Deserialize, Serialize};
 use serde_json::json;
 use tendermint_rpc::{
     query::{EventType, Query},
@@ -38,30 +31,6 @@ use tokio::{
     io::AsyncWriteExt,
 };
 use tonic::Request;
-
-#[derive(Debug)]
-struct Quote3ErrorWrapper(Quote3Error);
-
-impl fmt::Display for Quote3ErrorWrapper {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "Quote3Error: {:?}", self.0)
-    }
-}
-
-impl std::error::Error for Quote3ErrorWrapper {}
-
-impl From<Quote3Error> for Quote3ErrorWrapper {
-    fn from(error: Quote3Error) -> Self {
-        Quote3ErrorWrapper(error)
-    }
-}
-
-// TODO: import this from enclave or somewhere shared
-#[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct RunClearingMessage {
-    intents: BTreeMap<HexBinary, HexBinary>,
-    liquidity_sources: BTreeSet<LiquiditySource>,
-}
 
 #[derive(Clone, Debug, Parser)]
 #[command(version, about, long_about = None)]
@@ -166,8 +135,10 @@ async fn handler(
     let attestation = gramine_ias_request(quote.attestation, user).await?;
     let msg = RawAttestedMsgSansHandler(quote.msg);
 
-    let setoffs_msg =
-        ExecuteMsg::SubmitSetoffs::<DcapAttestation>(AttestedMsg { msg, attestation });
+    let setoffs_msg = ExecuteMsg::SubmitSetoffs::<RawEpidAttestation>(AttestedMsg {
+        msg,
+        attestation: attestation.into(),
+    });
 
     // Send setoffs to mtcs contract on chain
     let output =
@@ -233,7 +204,7 @@ async fn query_chain(
 async fn gramine_ias_request(
     attested_msg: Vec<u8>,
     user: &str,
-) -> Result<DcapAttestation, anyhow::Error> {
+) -> Result<EpidAttestation, anyhow::Error> {
     let ias_api_key = String::from("669244b3e6364b5888289a11d2a1726d");
     let ra_client_spid = String::from("51CAF5A48B450D624AEFE3286D314894");
     let quote_file = format!("/tmp/{}_test.quote", user);
@@ -260,42 +231,9 @@ async fn gramine_ias_request(
         return Err(anyhow!("Couldn't run gramine. {:?}", output));
     }
 
-    let quote: Quote = Quote3::try_from(fs::read(quote_file).await?).map_err(Quote3ErrorWrapper)?;
-    let report = fs::read_to_string(&report_file).await?;
-    let report_sig = fs::read_to_string(&report_sig_file).await?;
+    let report: ReportBody = serde_json::from_str(&fs::read_to_string(report_file).await?)?;
+    let report_sig_str = fs::read_to_string(report_sig_file).await?.replace('\r', "");
+    let report_sig: Binary = Binary::from_base64(report_sig_str.trim())?;
 
-    // Parse the collateral from the report and report_sig
-    let mut sgx_collateral = sgx_ql_qve_collateral_t::default();
-    #[allow(unsafe_code)]
-    let version = unsafe { sgx_collateral.__bindgen_anon_1.__bindgen_anon_1.as_mut() };
-    version.major_version = 3;
-    version.minor_version = 1;
-
-    // Set other fields of sgx_collateral
-    sgx_collateral.pck_crl_issuer_chain = report.as_ptr() as *mut i8;
-    sgx_collateral.root_ca_crl = report_sig.as_ptr() as *mut i8;
-    sgx_collateral.pck_crl = report_sig.as_ptr() as *mut i8;
-    sgx_collateral.tcb_info_issuer_chain = report.as_ptr() as *mut i8;
-    sgx_collateral.tcb_info = report.as_ptr() as *mut i8;
-    sgx_collateral.qe_identity_issuer_chain = report.as_ptr() as *mut i8;
-    sgx_collateral.qe_identity = report.as_ptr() as *mut i8;
-    sgx_collateral.pck_crl_issuer_chain_size = report.len() as u32;
-    sgx_collateral.root_ca_crl_size = report_sig.len() as u32;
-    sgx_collateral.pck_crl_size = report_sig.len() as u32;
-    sgx_collateral.tcb_info_issuer_chain_size = report.len() as u32;
-    sgx_collateral.tcb_info_size = report.len() as u32;
-    sgx_collateral.qe_identity_issuer_chain_size = report.len() as u32;
-    sgx_collateral.qe_identity_size = report.len() as u32;
-    sgx_collateral.pck_crl_issuer_chain = report.as_ptr() as *mut i8;
-    sgx_collateral.root_ca_crl = report_sig.as_ptr() as *mut i8;
-    sgx_collateral.pck_crl = report_sig.as_ptr() as *mut i8;
-    sgx_collateral.tcb_info_issuer_chain = report.as_ptr() as *mut i8;
-    sgx_collateral.tcb_info = report.as_ptr() as *mut i8;
-    sgx_collateral.qe_identity_issuer_chain = report.as_ptr() as *mut i8;
-    sgx_collateral.qe_identity = report.as_ptr() as *mut i8;
-
-    // Try to create Collateral from sgx_collateral
-    let collateral = Collateral::try_from(&sgx_collateral).expect("msg");
-
-    Ok(DcapAttestation::new(quote, collateral))
+    Ok(EpidAttestation::new(IASReport { report, report_sig }))
 }
