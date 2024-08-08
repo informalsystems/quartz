a18ae28c48f4a5f6e9ab04c47fc1164414e8aa4c
dusterbloom <32869278+dusterbloom@users.noreply.github.com>
2024-08-08 12:25:56 +0200

    feat: neutrond tendermint upgrades  (#119)
    Co-authored-by: hu55a1n1 <sufialhussaini@gmail.com>

--- apps/mtcs/enclave/src/prost/mtcs.rs
+++ apps/mtcs/enclave/src/prost/mtcs.rs
@@ -137,19 +137,17 @@ pub mod clearing_server {
     }
     #[derive(Debug)]
     pub struct ClearingServer<T: Clearing> {
-        inner: _Inner<T>,
+        inner: Arc<T>,
         accept_compression_encodings: EnabledCompressionEncodings,
         send_compression_encodings: EnabledCompressionEncodings,
         max_decoding_message_size: Option<usize>,
         max_encoding_message_size: Option<usize>,
     }
-    struct _Inner<T>(Arc<T>);
     impl<T: Clearing> ClearingServer<T> {
         pub fn new(inner: T) -> Self {
             Self::from_arc(Arc::new(inner))
         }
         pub fn from_arc(inner: Arc<T>) -> Self {
-            let inner = _Inner(inner);
             Self {
                 inner,
                 accept_compression_encodings: Default::default(),
@@ -212,7 +210,6 @@ pub mod clearing_server {
             Poll::Ready(Ok(()))
         }
         fn call(&mut self, req: http::Request<B>) -> Self::Future {
-            let inner = self.inner.clone();
             match req.uri().path() {
                 "/mtcs.Clearing/Run" => {
                     #[allow(non_camel_case_types)]
@@ -243,7 +240,6 @@ pub mod clearing_server {
                     let max_encoding_message_size = self.max_encoding_message_size;
                     let inner = self.inner.clone();
                     let fut = async move {
-                        let inner = inner.0;
                         let method = RunSvc(inner);
                         let codec = tonic::codec::ProstCodec::default();
                         let mut grpc = tonic::server::Grpc::new(codec)
@@ -265,8 +261,11 @@ pub mod clearing_server {
                         Ok(
                             http::Response::builder()
                                 .status(200)
-                                .header("grpc-status", "12")
-                                .header("content-type", "application/grpc")
+                                .header("grpc-status", tonic::Code::Unimplemented as i32)
+                                .header(
+                                    http::header::CONTENT_TYPE,
+                                    tonic::metadata::GRPC_CONTENT_TYPE,
+                                )
                                 .body(empty_body())
                                 .unwrap(),
                         )
@@ -285,16 +284,6 @@ pub mod clearing_server {
                 max_decoding_message_size: self.max_decoding_message_size,
                 max_encoding_message_size: self.max_encoding_message_size,
             }
-        }
-    }
-    impl<T: Clearing> Clone for _Inner<T> {
-        fn clone(&self) -> Self {
-            Self(Arc::clone(&self.0))
-        }
-    }
-    impl<T: std::fmt::Debug> std::fmt::Debug for _Inner<T> {
-        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-            write!(f, "{:?}", self.0)
         }
     }
     impl<T: Clearing> tonic::server::NamedService for ClearingServer<T> {
