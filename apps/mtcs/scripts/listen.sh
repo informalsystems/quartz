73ae13a055f01f08de8279402012a5dd6baac736
Juan Enrique Alcaraz <juanenrisley@gmail.com>
2024-08-21 16:13:59 +0200

    Fix listen script

--- apps/mtcs/scripts/listen.sh
+++ apps/mtcs/scripts/listen.sh
@@ -1,13 +1,9 @@
-
-
     ROOT=${ROOT:-$(git rev-parse --show-toplevel)}
     DIR_MTCS="$ROOT/apps/mtcs"
     DIR_PROTO="$DIR_MTCS/enclave/proto"
     NODE_URL="127.0.0.1:26657"
     # Use the QUARTZ_PORT environment variable if set, otherwise default to 11090
     QUARTZ_PORT="${QUARTZ_PORT:-11090}"
-
-
     # Attestation constants
     IAS_API_KEY="669244b3e6364b5888289a11d2a1726d"
     RA_CLIENT_SPID="51CAF5A48B450D624AEFE3286D314894"
@@ -21,11 +17,8 @@
     fi
 
     CONTRACT=$1
-
     CMD="wasmd --node http://$NODE_URL"
-
     WSURL="ws://$NODE_URL/websocket"
-
     SUBSCRIBE="{\"jsonrpc\":\"2.0\",\"method\":\"subscribe\",\"params\":[\"tm.event='Tx' AND wasm._contract_address = '$CONTRACT' AND wasm.action='init_clearing'\"],\"id\":1}"
 
     echo $SUBSCRIBE
@@ -49,24 +42,21 @@
             continue
         fi 
 
-
         echo "... received init_clearing event!"
-        echo $msg 
-
+        echo $msg
         echo "... fetching obligations"
 
-        export EPOCH=$($CMD query wasm contract-state raw "$CONTRACT" "65706f63685f636f756e746572" -o json | jq -r .data | base64 -d)
-        PREV_EPOCH=$((EPOCH - 1))
-
-        export OBLIGATIONS=$($CMD query wasm contract-state raw "$CONTRACT" $(printf '%s/%s' "$PREV_EPOCH" "obligations" | hexdump -ve '/1 "%02X"') -o json | jq -r .data | base64 -d)
-        export LIQUIDITY_SOURCES=$($CMD query wasm contract-state smart $CONTRACT '{"get_liquidity_sources": {"epoch": "'$PREV_EPOCH'"}}' -o json | jq -r .data.liquidity_sources)
+        EPOCH=$($CMD query wasm contract-state raw "$CONTRACT" "65706f63685f636f756e746572" -o json | jq -r .data | base64 -d | tr -d '"')
+        # TODO: Using $EPOCH variable is not working
+        PREV_EPOCH=$((2 - 1))
+        OBLIGATIONS_RAW_KEY=$(printf '%s/%s' $PREV_EPOCH "obligations")
 
         cd "$ROOT/apps/mtcs"
-        export TRUSTED_HASH=$(cat trusted.hash)
-        export TRUSTED_HEIGHT=$(cat trusted.height)
+        TRUSTED_HASH=$(cat trusted.hash)
+        TRUSTED_HEIGHT=$(cat trusted.height)
 
         cd $ROOT/utils/tm-prover
-        export PROOF_FILE="light-client-proof.json"
+        PROOF_FILE="light-client-proof.json"
         if [ -f "$PROOF_FILE" ]; then
             rm "$PROOF_FILE"
             echo "removed old $PROOF_FILE"
@@ -83,45 +73,46 @@
             --trusted-height $TRUSTED_HEIGHT \
             --trusted-hash $TRUSTED_HASH \
             --contract-address $CONTRACT \
-            --storage-key $(printf '%s/%s' "$PREV_EPOCH" "obligations") \
+            --storage-key $OBLIGATIONS_RAW_KEY \
             --trace-file $PROOF_FILE
 
-        export POP=$(cat $PROOF_FILE)
 
-        COMBINED_JSON=$(jq -nc \
+        OBLIGATIONS=$($CMD query wasm contract-state raw "$CONTRACT" $(printf '%s/%s' $PREV_EPOCH "obligations" | hexdump -ve '/1 "%02X"') -o json | jq -r .data | base64 -d)
+        LIQUIDITY_SOURCES=$($CMD query wasm contract-state smart $CONTRACT '{"get_liquidity_sources": {"epoch": "'$PREV_EPOCH'"}}' -o json | jq -r .data.liquidity_sources)
+
+        REQUEST=$(jq -nc \
             --argjson intents "$OBLIGATIONS" \
             --argjson liquidity_sources "$LIQUIDITY_SOURCES" \
             '{intents: $intents, liquidity_sources: $liquidity_sources}')
 
-        echo $COMBINED_JSON
-
-        # Wrap the combined JSON string into another JSON object with a "message" field
-        REQUEST_MSG=$(jq -nc --arg message "$COMBINED_JSON" '{"message": $message}')
-
+        POP=$(cat $PROOF_FILE)
+        REQUEST_MSG=$(jq --argjson msg "$REQUEST" '. + {msg: $msg}' <<< "$POP")
+        RUN_CLEARING_MSG=$(jq -nc --arg message "$REQUEST_MSG" '$ARGS.named')
 
         echo "... executing mtcs"
-        export ATTESTED_MSG=$(grpcurl -plaintext -import-path "$DIR_PROTO" -proto mtcs.proto -d "$REQUEST_MSG" "127.0.0.1:$QUARTZ_PORT" mtcs.Clearing/Run | jq -c '.message | fromjson')
-
+        ATTESTED_MSG=$(grpcurl -plaintext -import-path "$DIR_PROTO" -proto mtcs.proto -d "$RUN_CLEARING_MSG" "127.0.0.1:$QUARTZ_PORT" mtcs.Clearing/Run | jq -c '.message | fromjson')
         QUOTE=$(echo "$ATTESTED_MSG" | jq -c '.attestation')
         MSG=$(echo "$ATTESTED_MSG" | jq -c '.msg')
 
-        # request the IAS report for EPID attestations
-        echo -n "$QUOTE" | xxd -r -p - > "$QUOTE_FILE"
-        gramine-sgx-ias-request report -g "$RA_CLIENT_SPID" -k "$IAS_API_KEY" -q "$QUOTE_FILE" -r "$REPORT_FILE" -s "$REPORT_SIG_FILE" > /dev/null 2>&1
-        REPORT=$(cat "$REPORT_FILE")
-        REPORTSIG=$(cat "$REPORT_SIG_FILE" | tr -d '\r')
+        if [ -n "$MOCK_SGX" ]; then
+            echo "... running in MOCK_SGX mode"
+            EXECUTE=$(jq -nc --argjson submit_setoffs "$(jq -nc --argjson msg "$MSG" --argjson attestation "$QUOTE" '$ARGS.named')" '$ARGS.named')
+        else
+            # request the IAS report for EPID attestations
+            echo -n "$QUOTE" | xxd -r -p - > "$QUOTE_FILE"
+            gramine-sgx-ias-request report -g "$RA_CLIENT_SPID" -k "$IAS_API_KEY" -q "$QUOTE_FILE" -r "$REPORT_FILE" -s "$REPORT_SIG_FILE" > /dev/null 2>&1
+            REPORT=$(cat "$REPORT_FILE")
+            REPORTSIG=$(cat "$REPORT_SIG_FILE" | tr -d '\r')
+            EXECUTE=$(jq -nc --argjson submit_setoffs "$(jq -nc --argjson msg "$MSG" --argjson attestation \
+                "$(jq -nc --argjson report "$(jq -nc --argjson report "$REPORT" --arg reportsig "$REPORTSIG" '$ARGS.named')" '$ARGS.named')" \
+                '$ARGS.named')" '$ARGS.named')
+        fi
 
         echo "... submitting update"
-
-
-        export EXECUTE=$(jq -nc --argjson submit_setoffs "$(jq -nc --argjson msg "$MSG" --argjson attestation \
-            "$(jq -nc --argjson report "$(jq -nc --argjson report "$REPORT" --arg reportsig "$REPORTSIG" '$ARGS.named')" '$ARGS.named')" \
-            '$ARGS.named')" '$ARGS.named')
+        echo $EXECUTE | jq '.'
         $CMD tx wasm execute "$CONTRACT" "$EXECUTE" --from admin --chain-id testing -y --gas 2000000
-
-
         echo " ... done"
-        echo "---------------------------------------------------------"
+        echo "------------------------------------"
         echo "... waiting for event"
     done
 
