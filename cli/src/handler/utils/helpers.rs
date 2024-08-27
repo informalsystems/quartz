3de521c38604a742f6322744314a4bc48fb721a7
Daniel Gushchyan <39884512+dangush@users.noreply.github.com>
2024-08-26 18:28:11 -0700

    relay for gramine, untested

--- cli/src/handler/utils/helpers.rs
+++ cli/src/handler/utils/helpers.rs
@@ -2,6 +2,7 @@ use std::time::Duration;
 
 use anyhow::anyhow;
 use cosmrs::{AccountId, ErrorReport};
+use hex::decode;
 use quartz_common::{
     contract::msg::{
         execute::{session_create::RawSessionCreate, session_set_pub_key::RawSessionSetPubKey},
@@ -20,12 +21,19 @@ use tendermint::{block::Height, Hash};
 use tendermint_rpc::{
     endpoint::tx::Response as TmTxResponse, error::ErrorDetail, Client, HttpClient,
 };
-use tokio::fs;
+use tokio::{
+    fs::{self, File},
+    io::AsyncWriteExt,
+    process::Command,
+};
 use tracing::debug;
 use wasmd_client::{CliWasmdClient, WasmdClient};
 
 use super::types::RelayMessage;
-use crate::{config::Config, error};
+use crate::{
+    config::Config,
+    error::{self, Error},
+};
 
 pub fn wasmaddr_to_id(address_str: &str) -> Result<AccountId, anyhow::Error> {
     let (hr, _) = bech32_decode(address_str).map_err(|e| anyhow!(e))?;
@@ -89,17 +97,13 @@ pub async fn run_relay_rust<R: DeserializeOwned>(
 
                 let msg: RawSessionCreate = serde_json::from_value(attested_msg_json)?;
                 // Build the nested JSON structures
-                let inner_json = json!({
-                    "msg": msg,
-                    "attestation": quote
-                });
-
-                let quartz_json = json!({
-                    request_key: inner_json
-                });
-
                 let jsonify = json!({
-                    "quartz": quartz_json
+                    "quartz": {
+                        request_key: {
+                            "msg": msg,
+                            "attestation": quote
+                        }
+                    }
                 });
 
                 let query_result: R = serde_json::from_value(jsonify)?;
@@ -112,17 +116,13 @@ pub async fn run_relay_rust<R: DeserializeOwned>(
 
                 let msg: RawSessionSetPubKey = serde_json::from_value(attested_msg_json)?;
                 // Build the nested JSON structures
-                let inner_json = json!({
-                    "msg": msg,
-                    "attestation": quote
-                });
-
-                let quartz_json = json!({
-                    request_key: inner_json
-                });
-
                 let jsonify = json!({
-                    "quartz": quartz_json
+                    "quartz": {
+                        request_key: {
+                            "msg": msg,
+                            "attestation": quote
+                        }
+                    }
                 });
 
                 let query_result: R = serde_json::from_value(jsonify)?;
@@ -131,21 +131,53 @@ pub async fn run_relay_rust<R: DeserializeOwned>(
             }
         }
     }
-
-    todo!()
-    // if mock_sgx
-
-    // match against relaymessage variant and construct
-
     // else
 
     // docker
+    let quote_str = quote
+        .as_str()
+        .ok_or_else(|| Error::GenericErr("quote is not a string".to_string()))?;
 
-    // same request thing
+    let quote = decode(quote_str).map_err(|e| Error::GenericErr(e.to_string()))?;
+    let (report, report_sig) = run_docker_command(&quote).await?;
 
-    // if let RelayMessage::SessionSetPubKey(proof) = msg {
-    //     command.arg(proof);
-    // }
+    match relay_msg {
+        RelayMessage::Instantiate => {
+            // Construct the JSON structure for Instantiate
+            let msg: RawCoreInstantiate = serde_json::from_value(attested_msg_json)?;
+
+            let jsonify = json!({
+                "msg": msg,
+                "attestation": {
+                    "report": report,
+                    "reportsig": report_sig
+                }
+            });
+
+            let query_result: R = serde_json::from_value(jsonify)?;
+            return Ok(query_result);
+        }
+        RelayMessage::SessionCreate | RelayMessage::SessionSetPubKey(_) => {
+            // Convert request to snake_case
+            let request_key = relay_msg.to_string();
+
+            // Construct the JSON structure for SessionCreate or SessionSetPubKey
+            let jsonify = json!({
+                "quartz": {
+                    request_key: {
+                        "msg": attested_msg_json,
+                        "attestation": {
+                            "report": report,
+                            "reportsig": report_sig
+                        }
+                    }
+                }
+            });
+
+            let query_result: R = serde_json::from_value(jsonify)?;
+            return Ok(query_result);
+        }
+    }
 }
 
 // Note: time until tx commit is empiraclly 800ms on DO wasmd chain.
@@ -183,14 +215,14 @@ pub async fn block_tx_commit(client: &HttpClient, tx: Hash) -> Result<TmTxRespon
 }
 
 // Queries the chain for the latested height and hash
-pub fn query_latest_height_hash(node_url: &String) -> Result<(Height, Hash), error::Error> {
+pub fn query_latest_height_hash(node_url: &String) -> Result<(Height, Hash), Error> {
     let httpurl = Url::parse(&format!("http://{}", node_url))
-        .map_err(|e| error::Error::GenericErr(e.to_string()))?;
+        .map_err(|e| Error::GenericErr(e.to_string()))?;
     let wasmd_client = CliWasmdClient::new(httpurl);
 
     let (trusted_height, trusted_hash) = wasmd_client
         .trusted_height_hash()
-        .map_err(|e| error::Error::GenericErr(e.to_string()))?;
+        .map_err(|e| Error::GenericErr(e.to_string()))?;
 
     Ok((
         trusted_height.try_into()?,
@@ -202,7 +234,7 @@ pub async fn write_cache_hash_height(
     trusted_height: Height,
     trusted_hash: Hash,
     config: &Config,
-) -> Result<(), error::Error> {
+) -> Result<(), Error> {
     let height_path = config.cache_dir()?.join("trusted.height");
     fs::write(height_path.as_path(), trusted_height.to_string()).await?;
 
@@ -217,14 +249,58 @@ pub async fn read_cached_hash_height(config: &Config) -> Result<(Height, Hash), 
     let hash_path = config.cache_dir()?.join("trusted.hash");
 
     if !height_path.exists() {
-        return Err(error::Error::PathNotFile(height_path.display().to_string()));
+        return Err(Error::PathNotFile(height_path.display().to_string()));
     }
     if !hash_path.exists() {
-        return Err(error::Error::PathNotFile(hash_path.display().to_string()));
+        return Err(Error::PathNotFile(hash_path.display().to_string()));
     }
 
     let trusted_height: Height = fs::read_to_string(height_path.as_path()).await?.parse()?;
     let trusted_hash: Hash = fs::read_to_string(hash_path.as_path()).await?.parse()?;
 
     Ok((trusted_height, trusted_hash))
+}
+
+async fn run_docker_command(quote: &[u8]) -> Result<(String, String), Error> {
+    let dir = tempfile::tempdir()?;
+    let ias_api_key: &str = "669244b3e6364b5888289a11d2a1726d";
+    let ra_client_spid: &str = "51CAF5A48B450D624AEFE3286D314894";
+    let quote_file_path = dir.path().join("test.quote");
+    let datareport_file_path = dir.path().join("datareport");
+    let datareportsig_file_path = dir.path().join("datareportsig");
+
+    let mut quote_file = File::create(quote_file_path.clone()).await?;
+    quote_file.write_all(quote).await?;
+
+    let status = Command::new("docker")
+        .arg("run")
+        .arg("--rm")
+        .arg("-it")
+        .arg("-v")
+        .arg("/tmp:/tmp:rw")
+        .arg("gramineproject/gramine:1.7-jammy")
+        .arg(format!(
+            "gramine-sgx-ias-request report -g \"{}\" -k \"{}\" -q \"{}\" -r \"{}\" -s \"{}\" > /dev/null 2>&1",
+            ra_client_spid, ias_api_key, quote_file_path.display().to_string(), datareport_file_path.display().to_string(), datareportsig_file_path.display().to_string()
+        ))
+        .status()
+        .await
+        .map_err(|e| Error::GenericErr(e.to_string()))?;
+
+    if !status.success() {
+        return Err(Error::GenericErr(
+            "Failed to run docker command".to_string(),
+        ));
+    }
+
+    let report = fs::read_to_string(datareport_file_path)
+        .await
+        .map_err(|e| Error::GenericErr(e.to_string()))?;
+
+    let reportsig = fs::read_to_string(datareportsig_file_path)
+        .await
+        .map_err(|e| Error::GenericErr(e.to_string()))?
+        .replace("\r", "");
+
+    Ok((report, reportsig))
 }
