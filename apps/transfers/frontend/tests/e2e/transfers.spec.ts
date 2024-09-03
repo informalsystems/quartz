e219de9b760b728ffb93fbac59d015f52201e6ee
Juan Enrique Alcaraz <juanenrisley@gmail.com>
2024-09-03 11:52:25 +0200

    Adapt old tests with new UI flows

--- apps/transfers/frontend/tests/e2e/transfers.spec.ts
+++ apps/transfers/frontend/tests/e2e/transfers.spec.ts
@@ -6,10 +6,8 @@ test.beforeEach(async ({ page }) => {
 
 test.describe('Transfers', () => {
   test('app should render correctly', async ({ page }) => {
-    await test.expect(page.getByText('Balance:')).toBeVisible()
-  })
-
-  test('balance should be 0 at first', async ({ page }) => {
-    await test.expect(page.getByText('$0')).toBeVisible()
+    await test
+      .expect(page.getByRole('button', { name: /connect/i }))
+      .toBeVisible()
   })
 })
