7ef81d5907c4f9bcf1ceec26a1fb34ab23a7872b
Juan Enrique Alcaraz <juanenrisley@gmail.com>
2024-09-26 13:58:32 +0200

    Fix missing DefaultAttestor

--- apps/transfers/enclave/src/main.rs
+++ apps/transfers/enclave/src/main.rs
@@ -28,7 +28,7 @@ use cli::Cli;
 use quartz_common::{
     contract::state::{Config, LightClientOpts},
     enclave::{
-        attestor::{self, Attestor},
+        attestor::{self, Attestor, DefaultAttestor},
         server::{QuartzServer, WsListenerConfig},
     },
 };
