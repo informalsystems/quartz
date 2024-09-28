74a46196340d871057f1d6bea610ff54c86823a3
Daniel Gushchyan <39884512+dangush@users.noreply.github.com>
2024-09-28 13:25:51 -0700

    cargo.toml fixes

--- crates/enclave/quartz-enclave/src/attestor.rs
+++ crates/enclave/quartz-enclave/src/attestor.rs
@@ -102,7 +102,7 @@ impl Attestor for DcapAttestor {
             version.minor_version = 1;
 
             let mut root_crl =
-                include_bytes!("../../../cosmwasm/quartz-tee-ra/data/root_crl.der").to_vec();
+                include_bytes!("../../../contracts/quartz-tee-ra/data/root_crl.der").to_vec();
             root_crl.push(0);
             sgx_collateral.root_ca_crl = root_crl.as_ptr() as _;
             sgx_collateral.root_ca_crl_size = root_crl.len() as u32;
@@ -117,8 +117,8 @@ impl Attestor for DcapAttestor {
             sgx_collateral.pck_crl_issuer_chain = pck_crl_issuer_chain.as_ptr() as _;
             sgx_collateral.pck_crl_issuer_chain_size = pck_crl_issuer_chain.len() as u32;
 
-            let root_cert = include_str!("../../../cosmwasm/quartz-tee-ra/data/root_ca.pem");
-            let tcb_cert = include_str!("../../../cosmwasm/quartz-tee-ra/data/tcb_signer.pem");
+            let root_cert = include_str!("../../../contracts/quartz-tee-ra/data/root_ca.pem");
+            let tcb_cert = include_str!("../../../contracts/quartz-tee-ra/data/tcb_signer.pem");
             let mut tcb_chain = [tcb_cert, root_cert].join("\n").as_bytes().to_vec();
             tcb_chain.push(0);
             sgx_collateral.tcb_info_issuer_chain = tcb_chain.as_ptr() as _;
@@ -132,7 +132,7 @@ impl Attestor for DcapAttestor {
             sgx_collateral.qe_identity_issuer_chain_size = tcb_chain.len() as u32;
 
             const QE_IDENTITY_JSON: &str =
-                include_str!("../../../cosmwasm/quartz-tee-ra/data/qe_identity.json");
+                include_str!("../../../contracts/quartz-tee-ra/data/qe_identity.json");
             sgx_collateral.qe_identity = QE_IDENTITY_JSON.as_ptr() as _;
             sgx_collateral.qe_identity_size = QE_IDENTITY_JSON.len() as u32;
 
