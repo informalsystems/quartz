70ca340f8612edea17670d45b172636d7481a7a4
Peppi Littera <giuseppe@informal.systems>
2024-09-30 12:13:45 +0200

    Fixes to wss config

--- apps/transfers/enclave/src/wslistener.rs
+++ apps/transfers/enclave/src/wslistener.rs
@@ -131,8 +131,7 @@ async fn transfer_handler<A: Attestor>(
 ) -> Result<()> {
     let chain_id = &ChainId::from_str(&ws_config.chain_id)?;
     let httpurl = ws_config.node_url.clone();
-    let wasmd_client = CliWasmdClient::new(httpurl.clone());
-
+    let wasmd_client = CliWasmdClient::new(Url::parse(&httpurl)?);
     // Query chain
     // Get epoch, obligations, liquidity sources
     let resp: QueryResult<Vec<TransferRequest>> = wasmd_client
