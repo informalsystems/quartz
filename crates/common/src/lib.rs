4d5073293ee29120e5d8952552fdcd8a74ba345e
hu55a1n1 <sufialhussaini@gmail.com>
2024-10-01 13:37:23 +0200

    Rename quartz-enclave -> quartz-enclave-core

--- crates/common/src/lib.rs
+++ crates/common/src/lib.rs
@@ -1,6 +1,6 @@
 #[cfg(feature = "contract")]
 pub use quartz_contract_core as contract;
 #[cfg(feature = "enclave")]
-pub use quartz_enclave as enclave;
+pub use quartz_enclave_core as enclave;
 #[cfg(feature = "proto")]
 pub use quartz_proto::quartz as proto;
