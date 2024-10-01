9ecc02f2008612f4a13ca2b08d7dc90ccc0a9c3d
Elena San Miguel <elesmp@protonmail.com>
2024-10-01 22:13:39 +0200

    Run transfers app E2E tests in an Action, and backend with docker compose (#203)
    Co-authored-by: Juan Enrique Alcaraz <juanenrisley@gmail.com>

--- examples/transfers/frontend/playwright.config.ts
+++ examples/transfers/frontend/playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   timeout: 0,
-  globalTimeout: process.env.CI ? 5 * 60 * 1000 : undefined,
+  globalTimeout: process.env.CI ? 10 * 60 * 1000 : undefined,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
