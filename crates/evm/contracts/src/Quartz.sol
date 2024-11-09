65e0497c3e95e65d0d08a7fb923c54b2c544e10e
Dave Kaj <davidkajpust@informal.systems>
2024-11-08 22:54:17 -0500

    Forge fmt

--- crates/evm/contracts/src/Quartz.sol
+++ crates/evm/contracts/src/Quartz.sol
@@ -3,9 +3,9 @@ pragma solidity ^0.8.13;
 
 import "@automata-dcap/interfaces/IAttestation.sol";
 
-// QUESTION - We do not test that mr_enclave matches yet in cosmwasm. Should we do it here? 
-// QUESTION - I guess for the transfers app, or ping pong, they would actually inherit the Quartz contract? Right? 
-// this way whenever those functions are called on ping pong, they would increase the sequence number (I assume 
+// QUESTION - We do not test that mr_enclave matches yet in cosmwasm. Should we do it here?
+// QUESTION - I guess for the transfers app, or ping pong, they would actually inherit the Quartz contract? Right?
+// this way whenever those functions are called on ping pong, they would increase the sequence number (I assume
 // the handshake does not update the sequence number)
 // QUESTION - Contract address MIGHT be put into the config? (but I dont think that matters on the solidity side as its available globally in the contract)
 
@@ -27,23 +27,23 @@ contract Quartz {
         string chainID;
         uint256 trustedHeight;
         bytes32 trustedHash;
-        // etc.
     }
+    // etc.
 
     event SessionCreated(address indexed quartz);
     event PubKeySet(bytes32 indexed enclavePubKey);
 
     /**
-    * @notice Initializes the Quartz contract with the config, attests it's from a DCAP enclave,
-    * and emits an event for the host to listen to
-    * @dev On failure the contract will not deploy, and the user will lose the gas. The constructor
-    * is equivalent to the start of the handshake, and the session create, as the event emitted
-    * can be passed onto the host to share with the enclave
-    * @param _config The configuration object for the light client
-    * @param _quote The DCAP attestation quote provided by the enclave
-    * Emits a {SessionCreated} event upon successful verification.
-    * Reverts with an error message if `verifyAndAttestOnChain` fails.
-    */
+     * @notice Initializes the Quartz contract with the config, attests it's from a DCAP enclave,
+     * and emits an event for the host to listen to
+     * @dev On failure the contract will not deploy, and the user will lose the gas. The constructor
+     * is equivalent to the start of the handshake, and the session create, as the event emitted
+     * can be passed onto the host to share with the enclave
+     * @param _config The configuration object for the light client
+     * @param _quote The DCAP attestation quote provided by the enclave
+     * Emits a {SessionCreated} event upon successful verification.
+     * Reverts with an error message if `verifyAndAttestOnChain` fails.
+     */
     constructor(Config memory _config, bytes memory _quote) {
         config = _config;
         (bool success, bytes memory output) = attest.verifyAndAttestOnChain(_quote);
@@ -57,14 +57,14 @@ contract Quartz {
     }
 
     /**
-    * @notice Sets the session public key after verifying the attestation quote
-    * @dev This function is equivalent to cosmwasm setting of the session, without the nonce, since
-    * the nonce is no longer needed
-    * @param _pubKey The public key to be set for the session, provided by the enclave
-    * @param _quote The attestation quote to be verified, ensuring that the caller is authorized
-    * Emits a {PubKeySet} event upon successful setting of the public key
-    * Reverts with an error message if `verifyAndAttestOnChain` fails to verify the attestation
-    */
+     * @notice Sets the session public key after verifying the attestation quote
+     * @dev This function is equivalent to cosmwasm setting of the session, without the nonce, since
+     * the nonce is no longer needed
+     * @param _pubKey The public key to be set for the session, provided by the enclave
+     * @param _quote The attestation quote to be verified, ensuring that the caller is authorized
+     * Emits a {PubKeySet} event upon successful setting of the public key
+     * Reverts with an error message if `verifyAndAttestOnChain` fails to verify the attestation
+     */
     function setSessionPubKey(bytes32 _pubKey, bytes memory _quote) external {
         (bool success, bytes memory output) = attest.verifyAndAttestOnChain(_quote);
         if (success) {
@@ -79,19 +79,18 @@ contract Quartz {
     // TODO - Implement sequence number incrementing... but I assume we should have the transfers or ping pong app do this
 
     /**
-    * @notice Extracts the revert message from a failed external call's return data
-    * @param _output The raw return data from a failed external call
-    * @return The string representing the revert message
-    */
+     * @notice Extracts the revert message from a failed external call's return data
+     * @param _output The raw return data from a failed external call
+     * @return The string representing the revert message
+     */
     function _getRevertMessage(bytes memory _output) internal pure returns (string memory) {
         if (_output.length == 0) {
             return "Unknown error";
         }
-                assembly {
+        assembly {
             // Skip the first 4 bytes (error selector)
             _output := add(_output, 0x04)
         }
         return abi.decode(_output, (string));
     }
 }
-
