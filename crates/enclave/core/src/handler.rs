9e476a696c84cd1dc1d23ae1e3b0811ea19cff7b
Shoaib Ahmed <sufialhussaini@gmail.com>
2024-12-31 00:52:02 +0400

    Blanket Handler impl for all tonic requests

--- crates/enclave/core/src/handler.rs
+++ crates/enclave/core/src/handler.rs
@@ -1,3 +1,5 @@
+use tonic::{Request, Response};
+
 use crate::{attestor::Attestor, Enclave};
 
 pub type A<E> = <<E as Enclave>::Attestor as Attestor>::Attestation;
@@ -8,9 +10,25 @@ pub mod session_create;
 pub mod session_set_pubkey;
 
 #[async_trait::async_trait]
-pub trait Handler<Context> {
+pub trait Handler<Context>: Send + Sync + 'static {
     type Error;
     type Response;
 
-    async fn handle(&mut self, ctx: &mut Context) -> Result<Self::Response, Self::Error>;
+    async fn handle(self, ctx: &Context) -> Result<Self::Response, Self::Error>;
+}
+
+#[async_trait::async_trait]
+impl<T, C> Handler<C> for Request<T>
+where
+    T: Handler<C>,
+    C: Send + Sync,
+{
+    type Error = T::Error;
+    type Response = Response<T::Response>;
+
+    async fn handle(self, ctx: &C) -> Result<Self::Response, Self::Error> {
+        let request = self.into_inner();
+        let response = request.handle(ctx).await?;
+        Ok(Response::new(response.into()))
+    }
 }
