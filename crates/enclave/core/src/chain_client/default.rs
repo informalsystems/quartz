1e887b8d29ff4ef50fc38bbdb52245eb23e875a3
Shoaib Ahmed <sufialhussaini@gmail.com>
2025-01-05 02:19:41 +0400

    WIP impl DefaultChainClient

--- crates/enclave/core/src/chain_client/default.rs
+++ crates/enclave/core/src/chain_client/default.rs
@@ -1,11 +1,47 @@
-use cosmrs::AccountId;
+use cosmrs::{crypto::secp256k1::SigningKey, AccountId};
+use cw_client::{CwClient, GrpcClient};
+use reqwest::Url;
+use serde::de::DeserializeOwned;
 
 use crate::chain_client::ChainClient;
 
-#[derive(Clone, Debug, Default)]
-pub struct DefaultChainClient;
+pub struct DefaultChainClient {
+    grpc_client: GrpcClient,
+}
 
+impl DefaultChainClient {
+    pub fn new(signer: SigningKey, url: Url) -> Self {
+        DefaultChainClient {
+            grpc_client: GrpcClient::new(signer, url),
+        }
+    }
+}
+
+#[async_trait::async_trait]
 impl ChainClient for DefaultChainClient {
-    const CHAIN_ID: &'static str = "pion-1";
     type Contract = AccountId;
+    type Error = anyhow::Error;
+    type Proof = ();
+
+    async fn query_contract<R: DeserializeOwned + Default>(
+        &self,
+        contract: &Self::Contract,
+        query: String,
+    ) -> Result<R, Self::Error> {
+        self.grpc_client
+            .query_raw(&contract, query.to_string())
+            .await
+    }
+
+    async fn existence_proof(
+        &self,
+        _contract: &Self::Contract,
+        _storage_key: &str,
+    ) -> Result<Self::Proof, Self::Error> {
+        todo!()
+    }
+
+    async fn wait_for_blocks(&self, _blocks: u8) -> Result<Self::Proof, Self::Error> {
+        todo!()
+    }
 }
