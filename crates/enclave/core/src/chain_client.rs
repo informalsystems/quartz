bb7fad69a4f624198f46d12d2da21b52bd432023
Shoaib Ahmed <sufialhussaini@gmail.com>
2025-01-05 02:19:03 +0400

    Flesh out ChainClient

--- crates/enclave/core/src/chain_client.rs
+++ crates/enclave/core/src/chain_client.rs
@@ -1,7 +1,26 @@
+use std::fmt::Display;
+
+use serde::{de::DeserializeOwned, Serialize};
+
 pub mod default;
 
+#[async_trait::async_trait]
 pub trait ChainClient: Send + Sync + 'static {
-    const CHAIN_ID: &'static str;
+    type Contract: Send + Sync + 'static;
+    type Error: Display + Send + Sync + 'static;
+    type Proof: Serialize + Send + Sync + 'static;
 
-    type Contract;
+    async fn query_contract<R: DeserializeOwned + Default>(
+        &self,
+        contract: &Self::Contract,
+        query: String,
+    ) -> Result<R, Self::Error>;
+
+    async fn existence_proof(
+        &self,
+        contract: &Self::Contract,
+        storage_key: &str,
+    ) -> Result<Self::Proof, Self::Error>;
+
+    async fn wait_for_blocks(&self, blocks: u8) -> Result<Self::Proof, Self::Error>;
 }
