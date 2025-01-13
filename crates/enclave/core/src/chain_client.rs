81aacda907856c51f44bda7be263fcaaeca4be79
Shoaib Ahmed <sufialhussaini@gmail.com>
2025-01-13 16:50:14 +0400

    Add ChainClient::send_tx and assoc type Query

--- crates/enclave/core/src/chain_client.rs
+++ crates/enclave/core/src/chain_client.rs
@@ -9,11 +9,13 @@ pub trait ChainClient: Send + Sync + 'static {
     type Contract: Send + Sync + 'static;
     type Error: Display + Send + Sync + 'static;
     type Proof: Serialize + Send + Sync + 'static;
+    type Query: Serialize + Send + Sync + 'static;
+    type TxOutput: Send + Sync + 'static;
 
     async fn query_contract<R: DeserializeOwned + Default>(
         &self,
         contract: &Self::Contract,
-        query: String,
+        query: Self::Query,
     ) -> Result<R, Self::Error>;
 
     async fn existence_proof(
@@ -21,6 +23,14 @@ pub trait ChainClient: Send + Sync + 'static {
         contract: &Self::Contract,
         storage_key: &str,
     ) -> Result<Self::Proof, Self::Error>;
+
+    async fn send_tx<T: Serialize + Send + Sync>(
+        &self,
+        contract: &Self::Contract,
+        tx: T,
+        gas: u64,
+        fees: u128,
+    ) -> Result<Self::TxOutput, Self::Error>;
 
     async fn wait_for_blocks(&self, blocks: u8) -> Result<Self::Proof, Self::Error>;
 }
