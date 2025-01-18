836a08ca6806763d27eaac08ae60089626fbd5c2
Shoaib Ahmed <sufialhussaini@gmail.com>
2025-01-19 01:52:28 +0400

    Impl #[derive(UserData)]

--- examples/pingpong/contracts/src/msg.rs
+++ examples/pingpong/contracts/src/msg.rs
@@ -34,23 +34,11 @@ pub mod execute {
         pub message: HexBinary,
     }
 
+    #[derive(UserData)]
     #[cw_serde]
     pub struct Pong {
         pub pubkey: HexBinary,
         pub response: HexBinary,
-    }
-
-    // TODO: make macro
-    impl HasUserData for Pong {
-        fn user_data(&self) -> UserData {
-            let mut hasher = Sha256::new();
-            hasher.update(serde_json::to_string(&self).expect("infallible serializer"));
-            let digest: [u8; 32] = hasher.finalize().into();
-
-            let mut user_data = [0u8; 64];
-            user_data[0..32].copy_from_slice(&digest);
-            user_data
-        }
     }
 }
 
