9df6c829ab52afd800fe5b0521bccea0ea1440ba
Shoaib Ahmed <sufialhussaini@gmail.com>
2025-03-24 16:26:38 +0400

    Fix UserData derive macro
    So users don't have to import sha2 and serde_json crates

--- crates/contracts/core/derive/src/lib.rs
+++ crates/contracts/core/derive/src/lib.rs
@@ -10,15 +10,7 @@ pub fn user_data_derive(input: TokenStream) -> TokenStream {
     let expanded = quote! {
         impl ::quartz_contract_core::msg::execute::attested::HasUserData for #name {
             fn user_data(&self) -> ::quartz_contract_core::state::UserData {
-                use ::sha2::Digest;
-
-                let mut hasher = ::sha2::Sha256::new();
-                hasher.update(::serde_json::to_string(&self).expect("infallible serializer"));
-                let digest: [u8; 32] = hasher.finalize().into();
-
-                let mut user_data = [0u8; 64];
-                user_data[0..32].copy_from_slice(&digest);
-                user_data
+                ::quartz_contract_core::msg::execute::attested::user_data_json(self)
             }
         }
     };
