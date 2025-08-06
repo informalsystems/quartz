c915229b5d585a8d026aea48218e67e7ccd5525d
Shoaib Ahmed <sufialhussaini@gmail.com>
2025-08-07 01:32:21 +0530

    Define Backup trait

---
+++ crates/enclave/core/src/backup_restore.rs
@@ -0,0 +1,15 @@
+#[async_trait::async_trait]
+pub trait Backup {
+    /// The backup Config type specified by `Host`. (may include sealed file location)
+    type Config;
+    /// The error type returned by backup and restore ops.
+    type Error;
+
+    /// Persist the current state based on the specified config.
+    /// Must panic on failure.
+    async fn backup(&self, config: Self::Config);
+
+    /// Restore the backed-up state based on the specified config.
+    /// Must return `Ok(false)` if previous backup did not exist.
+    fn try_restore(&self, config: Self::Config) -> Result<bool, Self::Error>;
+}
